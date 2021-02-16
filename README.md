42ffd40d78dd045647196edc9b1d7d97d58e84d8
Ragavan Rajan <ragavan.rajan@downer.co.nz>
2021-02-16 16:37:13 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# decades
+ Simple python script that calculates how many decades you are old 
---
+++ decades.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you?\n"))
+#  whole number division using double slash 
+decades = age// 10 
+years = age % 10 
+print("You are " + str(decades) + " decades and " + str(years) + " years old." )
